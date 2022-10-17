b3fff8b07bb3a68fe825286e4df4dcd3afbb2a74
lixtar <lixtar00@gmail.com>
2022-10-17 06:27:28 +0300

    fix convertToPHPValue

--- src/EasyEnum.php
+++ src/EasyEnum.php
@@ -2,6 +2,7 @@
 
 namespace LIXTAR\EasyEnum;
 
+use BackedEnum;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
 use Exception;
@@ -37,16 +38,22 @@ abstract class EasyEnum extends Type
         return true;
     }
 
+    /**
+     * @throws Exception
+     */
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
         return sprintf("ENUM('%s')", implode("', '", $this->getValues()));
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform): ?string
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?BackedEnum
     {
-        return $value;
+        return $this->enumClassname::from($value);
     }
 
+    /**
+     * @throws Exception
+     */
     public function convertToDatabaseValue($value, AbstractPlatform $platform): string
     {
         if (!in_array($value, $this->getValues())) {
